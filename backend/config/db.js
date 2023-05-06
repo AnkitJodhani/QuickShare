8934a6d0de5f5228e6c5fec7e7f19cff381ae73f
ankit jodhani <ankitjodhani1903@gmail.com>
2023-05-06 11:59:24 +0530

    did as it is

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,14 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-
-        // database connection
-        const status = await mongoose.connect(process.env.MONGO_URL)
-        if (!status) {
-            return false
-        }
-        else {
-            console.log("Database connected successfully");
-            return true
-        }
-
-    }
-    catch (err) {
+const connectDB = ()=>{
+    // database connection
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        console.log("Database connected successfully");
+    })
+    .catch((err)=>{
         console.log(err);
-    }
+    })
 }
 
 module.exports = connectDB;
